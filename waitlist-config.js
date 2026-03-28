a94c61c2a3702d61a8f3d74954c5fed8d24df728
jlsqs <62663766+jlsqs@users.noreply.github.com>
2026-03-28 13:12:38 +0100

    Update waitlist-config.js

--- waitlist-config.js
+++ waitlist-config.js
@@ -5,9 +5,9 @@ export const WAITLIST_CONFIG = {
     // Target classes to monitor for availability
     TARGET_CLASSES: [
         {
-            name: 'hard training',
-            time: '08:35',
-            dayOfWeek: 4, // thursday
+            name: 'all for the butt',
+            time: '16:50',
+            dayOfWeek: 6, // thursday
             location: null, // Any location
             instructor: 'TBA', // Any instructor
             priority: 1
